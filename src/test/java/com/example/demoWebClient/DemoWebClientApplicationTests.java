863234b78b443ca83f6ef35a6a91666352a1dc59
charlyval <alonsomartincarlos@gmail.com>
2025-01-17 20:29:21 +0100

    Primer commit

---
+++ src/test/java/com/example/demoWebClient/DemoWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoWebClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
